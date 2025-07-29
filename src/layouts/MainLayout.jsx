0e98c501dc8c07778587c334dba85504b1e7f8c8
abid129456 <ahabid515@gmail.com>
2025-07-29 20:39:11 +0600

    added the 8

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-
 const MainLayout = () => {
   return (
     <>
@@ -13,5 +12,4 @@ const MainLayout = () => {
     </>
   );
 };
-
 export default MainLayout;

2d6a9b8cf32a79fc0a2df9bfa2afc2fcaa89d4bf
abid129456 <ahabid515@gmail.com>
2025-07-29 20:31:34 +0600

    added the second code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,3 @@
-// const Footer = () => {
-//   return (
-//     <footer className="bg-gray-100 text-center py-4 mt-10">
-//       <p className="text-sm text-gray-600">
-//         &copy; {new Date().getFullYear()} Tourist Guide. All rights reserved.
-//       </p>
-//     </footer>
-//   );
-// };
-
-// export default Footer;
-
-
 import React from "react";
 
 const Footer = () => {
